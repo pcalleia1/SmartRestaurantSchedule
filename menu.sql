b1c008d6da5b8e8c88ad80b235f5278c1e751232
Dr. Evil <xfu@gmail.com>
2017-02-19 10:14:16 -0500

    added menu.sql

---
+++ menu.sql
@@ -0,0 +1,13 @@
+# To run it:
+# mysql -u root -p < menu.sql
+# enter pwd: goodyear123!@#
+
+DROP DATABASE IF EXISTS menu_db;;
+CREATE DATABASE menu_db;;
+USE menu_db;;
+CREATE TABLE objects(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255),
+	val TEXT
+);
+INSERT INTO objects (name, val) VALUES ("menu", "INVALIDVAL");
