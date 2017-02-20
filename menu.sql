34d951a56d386f93ec8f7913ebe58b9da594a2da
Dr. Evil <xfu@gmail.com>
2017-02-20 08:55:37 -0500

    sipmle

--- menu.sql
+++ menu.sql
@@ -2,12 +2,14 @@
 # mysql -u root -p < menu.sql
 # enter pwd: goodyear123!@#
 
-DROP DATABASE IF EXISTS menu_db;;
-CREATE DATABASE menu_db;;
-USE menu_db;;
+DROP DATABASE IF EXISTS menu_db;
+CREATE DATABASE menu_db;
+USE menu_db;
 CREATE TABLE objects(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(255),
 	val TEXT
 );
 INSERT INTO objects (name, val) VALUES ("menu", "INVALIDVAL");
+
+
